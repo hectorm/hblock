35055175411f15efdbe88eeaaf04d55fda7ea369
Héctor Molinero Fernández <hector@molinero.dev>
2020-11-11 19:26:42 +0100

    Added support for an environment file

--- resources/tests/env.sh
+++ resources/tests/env.sh
@@ -11,13 +11,8 @@ SCRIPT_DIR="$(CDPATH='' cd -- "$(dirname -- "${0:?}")" && pwd -P)"
 
 # Set base environment.
 export HOSTNAME='hblock'
-export HBLOCK_OUTPUT_FILE='/dev/null'
-export HBLOCK_HEADER_FILE='builtin'
-export HBLOCK_FOOTER_FILE='builtin'
-export HBLOCK_SOURCES_FILE='builtin'
+export ETCDIR="${SCRIPT_DIR:?}/etc"
 export HBLOCK_SOURCES="file://${SCRIPT_DIR:?}/sources.txt"
-export HBLOCK_ALLOWLIST_FILE='builtin'
-export HBLOCK_DENYLIST_FILE='builtin'
 
 runInTestShell() {
 	${TEST_SHELL:-/bin/sh} -- "${@-}" 2>&1 ||:
